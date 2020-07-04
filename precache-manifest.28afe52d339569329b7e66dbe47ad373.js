2f08fa4eef60b648be8287600183e35e0dd57d44
SKJ <sripal.17jccs069@jietjodhpur.ac.in>
2020-07-04 11:55:23 +0530

    Initial Commit

---
+++ precache-manifest.28afe52d339569329b7e66dbe47ad373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168ac67a81cfbd57dea14433d1f20ffc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9ff39f757c023d4eac0",
+    "url": "./static/css/main.6ef7c94b.chunk.css"
+  },
+  {
+    "revision": "393285d3e1b6654e0cdb",
+    "url": "./static/js/2.96687d43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.96687d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ff39f757c023d4eac0",
+    "url": "./static/js/main.0bfb4906.chunk.js"
+  },
+  {
+    "revision": "9f85700d870dbef83877",
+    "url": "./static/js/runtime-main.62ad3b68.js"
+  }
+]);
